9631a8a2f84fa32eda22755677a1eabcaed8cdfb
VGunjan <CapsLock@9379>
2019-10-20 17:44:54 +0530

    Twitter Trending Hashtag

--- src/main/java/com/gunjan/PrcoessTweetFromKafkaAndWriteToInfluxDatabase.java
+++ src/main/java/com/gunjan/PrcoessTweetFromKafkaAndWriteToInfluxDatabase.java
@@ -7,35 +7,26 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.annotation.Nullable;
-
 import org.apache.flink.api.common.functions.AggregateFunction;
-import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.configuration.Configuration;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.TimeCharacteristic;
-import org.apache.flink.streaming.api.datastream.ConnectedStreams;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.IterativeStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
-import org.apache.flink.streaming.api.functions.co.KeyedCoProcessFunction;
-import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -43,53 +34,9 @@ import org.apache.flink.streaming.api.windowing.triggers.ContinuousEventTimeTrig
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
-import org.apache.flink.util.OutputTag;
 
 public class PrcoessTweetFromKafkaAndWriteToInfluxDatabase
 {
-    public static void main1(String[] args) throws Exception
-    {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        
-        DataStream<Long> someIntegers = env.generateSequence(0, 1000);
-        
-        IterativeStream<Long> iteration = someIntegers.iterate();
-        
-        DataStream<Long> minusOne = iteration.map(new MapFunction<Long,Long>()
-        {
-            @Override
-            public Long map(Long value) throws Exception
-            {
-                return value - 1;
-            }
-        });
-        
-        DataStream<Long> stillGreaterThanZero = minusOne.filter(new FilterFunction<Long>()
-        {
-            @Override
-            public boolean filter(Long value) throws Exception
-            {
-                return (value > 0);
-            }
-        });
-        
-        
-        iteration.closeWith(stillGreaterThanZero);
-        
-        DataStream<Long> lessThanZero = minusOne.filter(new FilterFunction<Long>()
-        {
-            @Override
-            public boolean filter(Long value) throws Exception
-            {
-                return (value <= 0);
-            }
-        });
-        
-        lessThanZero.print();
-        
-        env.execute("Twitter Streaming Example");
-    }
-    
     public static void main(String[] args) throws Exception
     {
         InfluxDBConfig influxDBConfig = InfluxDBConfig.builder("http://10.71.69.236:31948", "root", "root", "twittergraph")
@@ -107,99 +54,29 @@ public class PrcoessTweetFromKafkaAndWriteToInfluxDatabase
         env.setParallelism(1);
         //env.enableCheckpointing(5000);
         
-        DataStream<Tuple2<String,String>> controlKeyedStream = env.socketTextStream("10.71.69.60", 9999, "\n").map(controlEvent -> new Tuple2<String,String>("key", controlEvent))
-                .returns(TypeInformation.of(new TypeHint<Tuple2<String,String>>()
-                {
-                }))
-                .keyBy(0);
-        
-        
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "10.71.69.236:31440");
         properties.setProperty("group.id", "flink");
         
         FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>("admintome-test", new SimpleStringSchema(), properties);
         
-        DataStream<Tuple2<String,String>> tweetKeyedStream = env.addSource(consumer).map(tweetStr -> new Tuple2<String,String>("key", tweetStr))
-                .returns(TypeInformation.of(new TypeHint<Tuple2<String,String>>()
-                {
-                }))
-                .keyBy(0);
+        SingleOutputStreamOperator<Tweet> tweetSingleOutputStreamOperator = env
+                .addSource(consumer)
+                .map(new MapToTweet())
+                .assignTimestampsAndWatermarks(new TimeLagWatermarkGenerator());
         
-        ConnectedStreams<Tuple2<String,String>,Tuple2<String,String>> connectedStreams = tweetKeyedStream.connect(controlKeyedStream);
-        
-        final OutputTag<Tweet> outputTag = new OutputTag<Tweet>("side-output")
+        ProcessWindowFunction<Tuple2<String,Integer>,Tuple3<String,Integer,Long>,String,TimeWindow> processFucntion = new ProcessWindowFunction<Tuple2<String,Integer>,Tuple3<String,Integer,Long>,
+                String,TimeWindow>()
         {
+            @Override
+            public void process(String s, Context context, Iterable<Tuple2<String,Integer>> elements, Collector<Tuple3<String,Integer,Long>> out) throws Exception
+            {
+                elements.forEach(stringIntegerLongTuple3 -> out.collect(new Tuple3<>(stringIntegerLongTuple3.f0, stringIntegerLongTuple3.f1, context.window().getEnd())));
+            }
         };
-        
-        SingleOutputStreamOperator<Tweet> tweetSingleOutputStreamOperator = connectedStreams.process(new KeyedCoProcessFunction<String,Tuple2<String,String>,Tuple2<String,String>,Tweet>()
-        {
-            private ValueState<Boolean> stopped = null;
-            
-            @Override
-            public void open(Configuration config)
-            {
-                ValueStateDescriptor<Boolean> descriptor = new ValueStateDescriptor<>(
-                        "have-stopped-key",
-                        TypeInformation.of(new TypeHint<Boolean>()
-                        {
-                        }));
-                stopped = getRuntimeContext().getState(descriptor);
-                    descriptor.setQueryable("is-tweet-processing-stopped");
-            }
-            
-            @Override
-            public void processElement1(Tuple2<String,String> value, Context ctx, Collector<Tweet> out) throws Exception
-            {
-                try
-                {
-                    Tweet tweet = new MapToTweet().map(value.f1);
-                    if(tweet.getText() != null && tweet.getText().length() > 0)
-                    {
-                        if(stopped.value() != null && !stopped.value().booleanValue())
-                        {
-                            out.collect(tweet);
-                        }
-                        else
-                        {
-                            ctx.output(outputTag, tweet);
-                        }
-                    }
-                }
-                catch(Exception e)
-                {
-                    //Ignore
-                }
-            }
-            
-            @Override
-            public void processElement2(Tuple2<String,String> value, Context ctx, Collector<Tweet> out) throws Exception
-            {
-                if("true".equals(value.f1))
-                {
-                    stopped.update(Boolean.TRUE);
-                }
-                else
-                {
-                    stopped.update(Boolean.FALSE);
-                }
-            }
-        }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<Tweet>(Time.seconds(0))
-        {
-            
-            @Override
-            public long extractTimestamp(Tweet element)
-            {
-                return element.getTimestamp_ms();
-            }
-        });
-        
-        
-        
-        
         tweetSingleOutputStreamOperator.flatMap(new TokenizeTweetTextFlatMap())
-                .keyBy((KeySelector<Tuple2<String,Integer>,Object>)hashtag -> hashtag.f0)
-                .timeWindow(Time.seconds(300), Time.seconds(5)).sum(1)
+                .keyBy((KeySelector<Tuple2<String,Integer>,String>)stringIntegerLongTuple3 -> stringIntegerLongTuple3.f0)
+                .timeWindow(Time.seconds(300), Time.seconds(5)).aggregate(new CustomSumAggregator(), processFucntion)
                 .timeWindowAll(Time.seconds(300), Time.seconds(5)).maxBy(1)
                 .map(new MapToInfluxDBPoint()).addSink(new InfluxDBSink(influxDBConfig));
         
@@ -222,19 +99,6 @@ public class PrcoessTweetFromKafkaAndWriteToInfluxDatabase
                 .timeWindowAll(Time.seconds(1))
                 .sum(0)
                 .map(new TweetPerSecondCountInfluxDBPoint())
-                .addSink(new InfluxDBSink(influxDBConfig));
-    
-        DataStream<Tweet> unprocessedTweet = tweetSingleOutputStreamOperator.getSideOutput(outputTag);
-        unprocessedTweet.print();
-        
-        unprocessedTweet
-                .map(tweet -> new Tuple2<>(1, tweet.getTimestamp_ms()))
-                .returns(TypeInformation.of(new TypeHint<Tuple2<Integer,Long>>()
-                {
-                }))
-                .timeWindowAll(Time.seconds(1))
-                .sum(0)
-                .map(new UnprocessedTweetPerSecondCountInfluxDBPoint())
                 .addSink(new InfluxDBSink(influxDBConfig));
         
         env.execute("Twitter Streaming Example");
@@ -282,18 +146,17 @@ public class PrcoessTweetFromKafkaAndWriteToInfluxDatabase
     }
     
     
-    private static class MapToInfluxDBPoint extends RichMapFunction<Tuple2<String,Integer>,InfluxDBPoint>
+    private static class MapToInfluxDBPoint extends RichMapFunction<Tuple3<String,Integer,Long>,InfluxDBPoint>
     {
         @Override
-        public InfluxDBPoint map(Tuple2<String,Integer> trendingHashTag) throws Exception
+        public InfluxDBPoint map(Tuple3<String,Integer,Long> trendingHashTag) throws Exception
         {
             String table = "TrendingHashTag";
-            long timestamp = System.currentTimeMillis();
             HashMap<String,String> tags = new HashMap<>();
             HashMap<String,Object> fields = new HashMap<>();
             fields.put("hashtag", trendingHashTag.f0);
             fields.put("count", trendingHashTag.f1);
-            return new InfluxDBPoint(table, timestamp, tags, fields);
+            return new InfluxDBPoint(table, trendingHashTag.f2, tags, fields);
         }
     }
     
@@ -339,23 +202,21 @@ public class PrcoessTweetFromKafkaAndWriteToInfluxDatabase
         }
     }
     
-    private static class TweetAssignerWithPeriodicWatermarks implements AssignerWithPeriodicWatermarks<Tweet>
+    public static class TimeLagWatermarkGenerator implements AssignerWithPeriodicWatermarks<Tweet>
     {
-        int count = 0;
         
-        @Nullable
+        private final long maxTimeLag = 10000;
+        
         @Override
-        public Watermark getCurrentWatermark()
+        public long extractTimestamp(Tweet element, long previousElementTimestamp)
         {
-            if(count > 30000) return new Watermark(Long.MAX_VALUE);
-            return null;
+            return element.getTimestamp_ms();
         }
         
         @Override
-        public long extractTimestamp(Tweet tweet, long l)
+        public Watermark getCurrentWatermark()
         {
-            count++;
-            return tweet.getTimestamp_ms();
+            return new Watermark(System.currentTimeMillis() - maxTimeLag);
         }
     }
     
@@ -403,6 +264,33 @@ public class PrcoessTweetFromKafkaAndWriteToInfluxDatabase
                 }
             }
             out.collect(max);
+        }
+    }
+    
+    public static class CustomSumAggregator implements AggregateFunction<Tuple2<String,Integer>,Tuple2<String,Integer>,Tuple2<String,Integer>>
+    {
+        @Override
+        public Tuple2<String,Integer> createAccumulator()
+        {
+            return new Tuple2<String,Integer>("", 0);
+        }
+        
+        @Override
+        public Tuple2<String,Integer> add(Tuple2<String,Integer> stringIntegerLongTuple3, Tuple2<String,Integer> stringIntegerLongTuple32)
+        {
+            return new Tuple2<>(stringIntegerLongTuple3.f0, stringIntegerLongTuple3.f1 + stringIntegerLongTuple32.f1);
+        }
+        
+        @Override
+        public Tuple2<String,Integer> getResult(Tuple2<String,Integer> stringIntegerLongTuple3)
+        {
+            return stringIntegerLongTuple3;
+        }
+        
+        @Override
+        public Tuple2<String,Integer> merge(Tuple2<String,Integer> stringIntegerLongTuple3, Tuple2<String,Integer> acc1)
+        {
+            return new Tuple2<>(stringIntegerLongTuple3.f0, stringIntegerLongTuple3.f1 + acc1.f1);
         }
     }
 }
